eb9058e2c16aa0e458486c51649271f3778f1413
Christina <37964725+csuh34@users.noreply.github.com>
2021-08-27 18:45:46 -0500

    Create functions2edit.py

---
+++ functions2edit.py
@@ -0,0 +1,125 @@
+# 8/6/20 Hayung Suh
+#
+# Includes some code by Sujin Jung -> deleted for confidentiality
+
+# empty_png, plot_shap, predict_gain
+
+import dash_html_components as html
+import glob
+import os
+import pathlib
+import shap
+import joblib
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from modules import cleanser
+from modules import gainer
+from joblib import dump, load
+
+import base64
+import io
+
+# predict gain - uses input file, model 1 & 2
+def predict_gain(contents, filename, filename1, filename2):
+    naCleanser = cleanser.BaseDataCleanser()
+    naCleanser.debugprt(1)
+    # Load Data
+    contents = contents[0]
+    filename = filename[0]
+    npre_df = input_parse_contents(contents, filename)
+
+    lookup = pd.read_csv('lookup_v7.csv')  # cell 정보
+
+    ###
+    #
+    # deleted
+    #
+    ###
+    
+    # save result file
+    gainer_report.to_csv('gain_pred_result.csv')
+
+    tempdf = pd.read_csv('gain_pred_result.csv')
+    dfs = tempdf[['short_name', 'Region', 'band', 'interference', '_cell_iptgain_weighted_pred', '_cell_dlvolgain_pred']]
+    dfs = dfs.rename(columns={'Region': 'Circle', 'band': 'Band'})
+    return dfs
+
+# input file into df
+def input_parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        df = pd.read_feather(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    return df
+
+# empty png directory before updateing shap value plots
+def empty_png():
+    mypath = str(pathlib.Path('__file__').parent.absolute())
+    image_directory = mypath + '/png/'
+    for f in glob.glob('{}*.png'.format(image_directory)):
+        os.remove(f)
+
+def plot_shap(filename1, filename2, gaintype, shaplist):
+    
+    ###
+    #
+    # deleted
+    #
+    ###
+
+    return shap_values, selected_idx, x_test, target, explainer, test_df
+
+def summary(shap_values, selected_idx, x_test, target):
+    save = 1  # or 1
+
+    newcmp = plt.get_cmap('rainbow')
+
+    # 1. summary plot
+    plt.figure()
+    shap.summary_plot(shap_values[selected_idx], x_test.iloc[selected_idx], show=False)
+
+    for fc in plt.gcf().get_children():
+        for fcc in fc.get_children():
+            if hasattr(fcc, "set_cmap"):
+                fcc.set_cmap(newcmp)
+
+    if save:
+        plt.savefig('png/%s_summary_plot.png' % target, format='png', bbox_inches='tight', facecolor='white')
+
+
+
+def decision(explainer, shap_values, selected_idx, test_df, x_test, target):
+    save = 1  # or 1
+
+    newcmp = plt.get_cmap('rainbow')
+
+    # 2. decision plot
+    plt.figure()
+    shap.decision_plot(explainer.expected_value, shap_values[selected_idx],
+                       legend_labels=test_df.loc[selected_idx, '_uebin_left'].tolist(),
+                       feature_names=x_test.columns.tolist(), show=False, plot_color=newcmp)
+    if save:
+        plt.savefig('png/%s_decision_plot.png' % target, format='png', bbox_inches='tight', facecolor='white')
+
+
+
+def dependence(x_test, selected_idx, shap_values, target):
+    save = 1  # or 1
+
+    newcmp = plt.get_cmap('rainbow')
+
+    # 3. dependence plots
+    
+    ###
+    #
+    # deleted
+    #
+    ###
